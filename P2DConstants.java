269b06fe4ff415278f70de78cd110a688000344c
Michael Widler <MichaelWidler@Yahoo.com>
2020-09-17 12:38:12 -0700

    Remove constants interface

--- P2DConstants.java
+++
@@ -1,8 +0,0 @@
-package Physics2D;
-
-public interface P2DConstants
-{
-
-   public static final int WATER_PHYSICS_INDEX = P2DManager.DEFAULT_PHYSICS_INDEX + 1;
-   public static final int ICE_PHYSICS_INDEX = WATER_PHYSICS_INDEX + 1;
-}
