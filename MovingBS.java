44d2f517ac5e20ed862c6cecd5af67df92c0db3e
Michael Widler <MichaelWidler@Yahoo.com>
2020-09-21 12:44:25 -0700

    implement MovingBS

---
+++ MovingBS.java
@@ -0,0 +1,23 @@
+/*
+
+   Interface for moving collision objects
+
+*/
+
+package Physics2D;
+
+public interface MovingBS
+{
+	public void setXSpeed(int x)
+	public void setYSpeed(int y)
+   public void setSpeed(int x, int y)
+	public void setAffectedByGravity(boolean a)
+	public void setCorporeal(boolean c)
+   
+	public int getXSpeed();
+	public int getYSpeed();
+	public boolean isAffectedByGravity();
+	public boolean isCorporeal();
+   
+   public void applyImpulse(int x, int y);
+}
