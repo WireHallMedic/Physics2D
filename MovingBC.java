b05e3450af9c64f148a758755e3b15569a01335a
Michael Widler <MichaelWidler@Yahoo.com>
2020-09-23 07:42:04 -0700

    rm circle test

--- MovingBC.java
+++ MovingBC.java
@@ -54,6 +54,53 @@ public class MovingBC extends BoundingCircle implements MovingBS
    
    public void doPhysics(GeometryBlock[][] geoMap)
    {
-      return;
+      int localGravity = getGravity(geoMap);
+      // set prospective position
+      if(affectedByGravity)
+         ySpeed = Math.min(ySpeed + P2DManager.getGravity(localGravity), 
+                           P2DManager.getTerminalVelocity(localGravity));
+      stepOriginX = originX + xSpeed;
+      stepOriginY = originY + ySpeed;
+      
+      boolean stopHoriz = false;
+      boolean stopVert = false;
+      
+      // corporeal objects check if they would collide with level geometry, stopping if they would.
+      // if moving diagionally, one tile is checked twice, but this is fast enough it shouldn't be
+      // a problem
+      if(corporeal)
+      {
+         // check and resolve horizontal movement
+         int snapLocX = getHorizSnapLoc(stepOriginX, originY, geoMap);
+         if(snapLocX != -1)
+         {
+            stepOriginX = snapLocX;
+         }
+
+         // check and resolve vertical movement
+         int snapLocY = getVertSnapLoc(stepOriginX, stepOriginY, geoMap);
+         if(snapLocY != -1)
+         {
+            stepOriginY = snapLocY;
+         }
+         
+         // check if you're bumping into any walls
+         updateSensors(stepOriginX, stepOriginY, geoMap);
+         
+         // stop movement if bumping into wall on that vector
+         if(xSpeed > 0 && rightSensor)
+            xSpeed = 0;
+         else if(xSpeed < 0 && leftSensor)
+            xSpeed = 0;
+         if(ySpeed > 0 && bottomSensor)
+            ySpeed = 0;
+         else if(ySpeed < 0 && topSensor)
+            ySpeed = 0;
+      }
+      
+      // set to final position. Directly accessing because of tile/millitile conversion.
+      originX = stepOriginX;
+      originY = stepOriginY;
+
    }
 }
